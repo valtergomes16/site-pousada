3d56998f200869217a178fb1fecbc62b2729fede
Valter Gomes Júnior <116452952+valtergomes16@users.noreply.github.com>
2023-01-04 18:21:41 -0300

    menuzinho

--- menu.js
+++ menu.js
@@ -10,3 +10,29 @@ function clickMenu(){
         hello.style.display = 'none'
     }
 }
+
+function clickMenu1(){
+    if(hamburguer.style.display == 'block'){
+        hamburguer.style.display = 'none'
+        main.style.display = 'block'
+        maincontato.style.display = 'block'
+    }
+    else{
+        hamburguer.style.display = 'block'
+        main.style.display = 'none'
+        maincontato.style.display = 'none'
+    }
+}
+
+function clickMenu2(){
+    if(hamburguer.style.display == 'block'){
+        hamburguer.style.display = 'none'
+        main.style.display = 'block'
+        mainquarto.style.display = 'block'
+    }
+    else{
+        hamburguer.style.display = 'block'
+        main.style.display = 'none'
+        mainquarto.style.display = 'none'
+    }
+}
